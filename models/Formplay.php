4a1c40743ca3572ce83827beb232d9f543569974
Loveorigami <loveorigami@mail.ru>
2016-08-17 18:59:06 +0300

    del constraint trait

--- models/Formplay.php
+++ models/Formplay.php
@@ -2,7 +2,6 @@
 namespace lo\modules\playground\models;
 
 use lo\core\db\TActiveRecord;
-use lo\core\rbac\IPermission;
 use Yii;
 
 /**
@@ -13,9 +12,8 @@ use Yii;
 
 class Formplay extends TActiveRecord
 {
-    use \lo\modules\eav\EavTrait;
+    //use \lo\modules\eav\EavTrait;
 
-    public $tplDir = '@lo/modules/playground/modules/admin/views/formplay/tpl/';
     public $eav;
 
     public static function tableName()
@@ -28,19 +26,20 @@ class Formplay extends TActiveRecord
      */
     public function metaClass()
     {
-        return FormplayMeta::class;
+        return FormplayMeta::className();
     }
 
     /**
      * @return \yii\db\ActiveQuery
-     */
+     
     public function getEavAttributes()
     {
         return \lo\modules\eav\models\EavAttribute::find()
             ->joinWith('entity')
             ->where([
                 'categoryId' => 0, // можно привязать к разной категории
-                'entityModel' => $this::class
+                'entityModel' => $this::className()
             ]);
     }
+	*/
 }
